cf3184efb357af354fdc95cdb3d2c78b2d18281f
swj <1061891980@qq.com>
2022-03-09 00:14:53 +0800

    配置linst

---
+++ .eslintrc.js
@@ -0,0 +1,22 @@
+// eslint-disable-next-line no-undef
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: [
+    'plugin:@typescript-eslint/recommended',
+    'plugin:prettier/recommended',
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  plugins: ['@typescript-eslint', 'prettier'],
+  rules: {
+    semi: ['error', 'always'],
+    quotes: ['error', 'single'],
+    'prettier/prettier': 'error',
+  },
+};
